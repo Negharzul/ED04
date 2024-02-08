d51a7122e79e6ea275b4ce0d2127c82449e7d24e
Antonio <636166@alu.murciaeduca.es>
2024-02-08 21:24:10 +0100

    Modificación ejercicio 3.

--- src/antonio/git/AntonioGIT.java
+++ src/antonio/git/AntonioGIT.java
@@ -14,7 +14,15 @@ public class AntonioGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Declaro un array de integers y lo muestro por consola con un método.
+        int[] m=new int[]{-2,0,20,15,7,-3};
+        
+        ImprimirArray(m);
+        
     }
     
+    public static void ImprimirArray(int[] m){
+        for (int i=0;i<m.length;i++) System.out.println(m[i]);
+    }
 }
