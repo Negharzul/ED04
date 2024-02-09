23729e55214705cbedc7a8f2f1c0332e21b04159
Antonio <636166@alu.murciaeduca.es>
2024-02-09 12:50:01 +0100

    Modificación del código para ejercicio 10.

--- src/antonio/git/AntonioGIT.java
+++ src/antonio/git/AntonioGIT.java
@@ -23,8 +23,8 @@ public class AntonioGIT {
         //Imprimo la suma de todos los valores del array.
         System.out.println(sumaArray(m));
         
-        //imprimo un valor aleatorio dentro del array.
-        System.out.println(aleatorioArray(m));
+        //imprimo la suma de 2 valores obtenidos aleatoriamente dentro del array.
+        System.out.println(aleatorioArray(m)+aleatorioArray(m));
         
     }
     
